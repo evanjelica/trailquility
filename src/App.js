fc37029930713069d6988cdddaf7443913768b3b
Anjelica Avorque <anjelica.avorque@gmail.com>
2021-11-13 16:53:02 -0600

    Installed Axios for API calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Landing from './components/Landing';
@@ -7,22 +6,6 @@ import ModalWindow from './components/ModalWindow';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-           <code>trailquility</code> 
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div>
         <Landing/>
         <ModalWindow/>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../css/Landing.css';
+
 import Typist from 'react-typist';
 
 export default function Landing(){
