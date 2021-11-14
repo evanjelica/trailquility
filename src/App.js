e2bd9f05dd0a9f473da779f262d6df84f1dbbe73
Anjelica Avorque <anjelica.avorque@gmail.com>
2021-11-13 23:21:15 -0600

    Removed footer component and added search page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 
 import Landing from './components/Landing';
 import ModalWindow from './components/ModalWindow';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <div>
         <Landing/>
-
       </div>
     </div>
   );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,22 +1,34 @@
-import React, {VFC} from 'react';
+import React from 'react';
+import Button from '@mui/material/Button';
+
 import '../css/Landing.css';
 
 
 export default function Landing(){
     return(
     <div className="video-background">
+
         <video autoPlay="autoplay" loop="loop" muted id="video">
             <source src="/video/videoBackground.mp4" type="video/mp4"></source>
         </video>
+
         <div className="overlay">
             <div className="header">
+
                 <h1>trailquility.</h1>
+
                 <div className="glass-container">
-                    <p>Powered by the National Park Service API. Trailquility let's you search up National Parks across the country!</p>
+                    <p className="glass-font">Powered by the National Park Service API. Trailquility let's you search up National Parks across the country!</p>
+                    <Button  color="success" variant="contained">Find parks near you!</Button>
                 </div>
             </div>
-        </div>
-    </div>
 
+            <div className="footer">
+                <p>Entry for Capital One Software Engineering Summit 2021</p>
+                <p>Built with React by <a href="https://evanjelica.github.io/" target="_blank">Anjelica Avorque</a></p>
+            </div>
+        </div>
+        
+    </div>
     )
 };
